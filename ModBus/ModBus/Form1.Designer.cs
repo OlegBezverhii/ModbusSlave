6389b28c5878fd32659c0293b72cee1d51c064b7
Oleg Bezverhii <oleg_bezverhii@mail.ru>
2021-01-26 19:03:51 +0900

    Add files via upload
    First app

---
+++ ModBus/ModBus/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace ModBus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.numericAdress = new System.Windows.Forms.NumericUpDown();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericBits = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericAdress)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBits)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericBits);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox5);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.numericAdress);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 165);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Параметры";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Скорость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Адрес";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Порт";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "None",
+            "OnePointFive"});
+            this.comboBox5.Location = new System.Drawing.Point(157, 95);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(57, 21);
+            this.comboBox5.TabIndex = 5;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "None",
+            "Even",
+            "Mark",
+            "Odd",
+            "Space"});
+            this.comboBox4.Location = new System.Drawing.Point(81, 95);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(57, 21);
+            this.comboBox4.TabIndex = 4;
+            // 
+            // numericAdress
+            // 
+            this.numericAdress.Location = new System.Drawing.Point(91, 43);
+            this.numericAdress.Name = "numericAdress";
+            this.numericAdress.Size = new System.Drawing.Size(47, 20);
+            this.numericAdress.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200"});
+            this.comboBox2.Location = new System.Drawing.Point(157, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(64, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(63, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // numericBits
+            // 
+            this.numericBits.Location = new System.Drawing.Point(10, 96);
+            this.numericBits.Name = "numericBits";
+            this.numericBits.Size = new System.Drawing.Size(59, 20);
+            this.numericBits.TabIndex = 12;
+            this.numericBits.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(289, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(346, 165);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(16, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 68);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1983.93, 2013.93, 123.456, 458.987";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericAdress)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBits)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.NumericUpDown numericAdress;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericBits;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ModBus/ModBus/Form1.cs
@@ -0,0 +1,138 @@
+﻿using Modbus.Device;
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace ModBus
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort sp = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Для каждого ComboBoxa указываю значение по умолчанию, надо потом будет дать осмысленные названия
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 3;
+            comboBox4.SelectedIndex = 0;
+            comboBox5.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Считываю название COM порта - пока сделан выбор, потом можно переделать в текстовый формат
+            string comport = comboBox1.SelectedItem.ToString();
+            //MessageBox.Show(comport);
+
+            //Скорость передачи данных
+            int velocity = Int32.Parse(comboBox2.SelectedItem.ToString());
+            //MessageBox.Show(velocity.ToString());
+
+            //Четность
+            string Paritys = comboBox4.SelectedItem.ToString();
+            //MessageBox.Show(Paritys);
+            Parity OutParity = Parity.None;
+
+            switch(Paritys)
+            {
+                case "Even":
+                    OutParity = Parity.Even;
+                    break;
+                case "Mark":
+                    OutParity = Parity.Mark;
+                    break;
+                case "None":
+                    OutParity = Parity.None;
+                    break;
+                case "Odd":
+                    OutParity = Parity.Odd;
+                    break;
+                case "Space":
+                    OutParity = Parity.Space;
+                    break;
+            }
+
+            //Битность (7,8)
+            //int bytes = Int32.Parse(comboBox2.SelectedItem.ToString());
+            int bytes = Convert.ToInt32(numericBits.Value);
+            //MessageBox.Show(bytes.ToString());
+
+            //Число стоповых битов
+            string StopBit = comboBox5.SelectedItem.ToString();
+            StopBits OutBits = StopBits.One;
+
+            switch (StopBit)
+            {
+                case "One":
+                    OutBits = StopBits.One;
+                    break;
+                case "OnePointFive":
+                    OutBits = StopBits.OnePointFive;
+                    break;
+                case "Two":
+                    OutBits = StopBits.Two;
+                    break;
+                case "None":
+                    OutBits = StopBits.None;
+                    break;
+            }
+
+
+            try
+            {
+                sp = new SerialPort(comport, velocity, OutParity, bytes, OutBits);
+                sp.Open();
+                MessageBox.Show("Порт открыт");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.Close();
+            MessageBox.Show("Порт закрыт");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Считываю Адрес устройства, к которому мы подсоединяемся
+                byte SlaveID = Convert.ToByte(numericAdress.Value);
+                ushort startAdress = 0;
+                ushort numOfPoints = 1;
+
+                //int adress = Convert.ToInt32(numericAdress.Value);
+                //MessageBox.Show(adress.ToString());
+
+                //ModbusSerialSlave slave = ModbusSerialSlave.CreateRtu(SlaveID, sp);
+                /*string[] strArr = textBox1.Text.Split(',');
+                float[] floatArr = new float[strArr.Length];
+                for (int i=0; i< strArr.Length; i++)
+                {
+                   floatArr[i] = float.Parse(strArr[i]);
+                }
+
+                ushort[] registers = mod*/
+
+
+                ModbusSerialMaster master = ModbusSerialMaster.CreateRtu(sp);
+                ushort[] holding_register = master.ReadHoldingRegisters(SlaveID, startAdress, numOfPoints);
+                MessageBox.Show(holding_register.ToString());
+                //Console.WriteLine(holding_register);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
